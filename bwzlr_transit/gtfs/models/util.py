a1384768dd1f1231eb70b616d707f7ba5fff40e3
Ben Wiswell <benjwiswell@gmail.com>
2025-03-30 01:06:16 -0400

    feat: remove pandas dependency

--- bwzlr_transit/gtfs/models/util.py
+++ bwzlr_transit/gtfs/models/util.py
@@ -9,6 +9,7 @@ T = TypeVar('T', object, object)
 KeyGetter = Callable[[T], str]
 
 
+
 def load_list (path: str, schema: m.Schema) -> list[T]:
     '''
     Reads a CSV file and returns a list of deserialized records.
@@ -23,12 +24,19 @@ def load_list (path: str, schema: m.Schema) -> list[T]:
         records (list[T]):
             a list of deserialized records
     '''
-    df = pd.read_csv(path, dtype=str).fillna('')
-    json = df.to_dict(orient='records')
-    values = []
-    for rec in json:
-        values.append(schema.load(rec))
-    return values
+    data: list[str]
+    with open(path, 'r') as file:
+        data = file.readlines()
+
+    header = data[0].rstrip().split(',')
+    data = [l.rstrip().split(',') for l in data[1:]]
+
+    return [
+        schema.load({ 
+            head: val 
+            for head, val in zip(header, values) 
+        }) for values in data
+    ]
 
     
 def load_table (
