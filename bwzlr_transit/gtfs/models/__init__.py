0fcee0f4f0efd2e01412d4e40edc69e13844a230
Ben Wiswell <benjwiswell@gmail.com>
2025-03-29 22:13:29 -0400

    feat: improve docstrings in Agency

---
+++ bwzlr_transit/gtfs/models/__init__.py
@@ -0,0 +1,3 @@
+from .agency import Agency, AGENCY_SCHEMA
+from .calendar_date import CalendarDate, ExceptionType, CALENDAR_DATE_SCHEMA
+from .feed import Feed, FEED_SCHEMA
--- bwzlr_transit/gtfs/models/agency.py
+++ bwzlr_transit/gtfs/models/agency.py
@@ -8,63 +8,50 @@ import marshmallow as m
 @dataclass
 class Agency:
     '''
-    Identifies a transit brand which is often synonymous with a transit agency. 
-    Note that in some cases, such as when a single agency operates multiple 
-    separate services, agencies and brands are distinct. This module uses the 
-    term "agency" in place of "brand". A dataset may contain data from multiple
-    agencies.
+    A GTFS dataclass model for records found in `agency.txt`. Identifies a 
+    transit agency.
+
+    Attributes:
+        id (str): the unique ID of the transit agency
+        email (Optional[str]): the customer service email of the transit agency
+        fare_url (Optional[str]): the URL of a fare or ticket website for the \
+        transit agency
+        lang (str): the primary language used by the transit agency
+        name (str): the name of the transit agency
+        phone (str): the voice telephone number for the transit agency
+        timezone (str): the timezone where the transit agency is located
+        url (str): the URL of the transit agency
     '''
     
     # Model ID
     id: str = d.field(m.fields.String(data_key='agency_id'))
-    '''Unique identifier of the transit agency.'''
+    '''the unique ID of the transit agency'''
     
     # Required fields
     name: str = d.field(m.fields.String(data_key='agency_name'))
-    '''Full name of the transit agency.'''
+    '''the name of the transit agency'''
     timezone: str = d.field(m.fields.String(data_key='agency_timezone'))
-    '''
-    Timezone where the transit agency is located. If multiple agencies are 
-    specified in the dataset, each must have the same `agency_timezone`.
-    '''
+    '''the timezone where the transit agency is located'''
     url: str = d.field(m.fields.String(data_key='agency_url'))
-    '''URL of the transit agency.'''
+    '''the URL of the transit agency'''
 
     # Optional fields
     email: Optional[str] = d.field(
         m.fields.String(data_key='agency_email', missing=None)
     )
-    '''
-    Email address actively monitored by the agency's customer service 
-    department. This email address should be a direct contact point where 
-    transit riders can reach a customer service representative at the agency.
-    '''
+    '''the customer service email of the transit agency'''
     fare_url: Optional[str] = d.field(
         m.fields.String(data_key='agency_fare_url', missing=None)
     )
-    '''
-    URL of a web page where a rider can purchase tickets or other fare 
-    instruments for that agency, or a web page containing information about 
-    that agency's fares.
-    '''
+    '''the URL of a fare or ticket website for the transit agency'''
     lang: Optional[str] = d.field(
         m.fields.String(data_key='agency_lang', missing=None)
     )
-    '''
-    Primary language used by this transit agency. Should be provided to help 
-    GTFS consumers choose capitalization rules and other language-specific 
-    settings for the dataset.
-    '''
+    '''the primary language used by the transit agency'''
     phone: Optional[str] = d.field(
         m.fields.String(data_key='agency_phone', missing=None)
     )
-    '''
-    A voice telephone number for the specified agency. This field is a string 
-    value that presents the telephone number as typical for the agency's 
-    service area. It may contain punctuation marks to group the digits of the 
-    number. Dialable text (for example, TriMet's "503-238-RIDE") is permitted,
-    but the field must not contain any other descriptive text.
-    '''
+    '''the voice telephone number for the transit agency'''
 
 
-AGENCY_SCEMA = d.schema(Agency)
+AGENCY_SCHEMA = d.schema(Agency)
