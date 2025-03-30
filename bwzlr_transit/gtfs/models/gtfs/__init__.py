f14f6b942ada2cb920474d88499aca438dbcd193
Ben Wiswell <benjwiswell@gmail.com>
2025-03-29 23:48:50 -0400

    feat: update Calendar record model

--- bwzlr_transit/gtfs/models/gtfs/__init__.py
+++ bwzlr_transit/gtfs/models/gtfs/__init__.py
@@ -1,3 +1,4 @@
 from .agency import Agency, AGENCY_SCHEMA
+from .calendar import Calendar, CALENDAR_SCHEMA
 from .calendar_date import CalendarDate, ExceptionType, CALENDAR_DATE_SCHEMA
 from .feed import Feed, FEED_SCHEMA
